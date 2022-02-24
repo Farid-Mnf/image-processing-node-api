79533bc4978535cf1d1316d86144d03feaf4168a
Farid-Mnf <faridggl100@gmail.com>
2022-02-25 00:12:31 +0200

    refactoring invalid query params

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,33 @@ const app = express();
 const port = 3000;
 const rootPath = process.env["INIT_CWD"];
 
-app.get("/api", (req, res) => {
+
+const middleware = (req: express.Request, res: express.Response, next: Function) => {
+  const queryString: string = req.query.image + "  " + req.query.width + "  " + req.query.height;
+  console.log(queryString);
+
+  const errorMsg = "missing or invalid image data";
+
+  if (req.query.image == undefined
+    || req.query.width == undefined
+    || req.query.height == undefined) {
+    res.send(errorMsg);
+  } else {
+    const image = req.query.image as string;
+    const width = req.query.width as string;
+    const height = req.query.height as string;
+
+    if(image.trim().length == 0 || width.trim().length == 0 || height.trim().length == 0){
+      res.send(errorMsg);
+    }else if()
+
+  }
+
+}
+
+app.get("/api", middleware, (req: express.Request, res: express.Response) => {
+  res.send("api connected");
+  /*
   if (findCached(req)) {
     res.sendFile(
       rootPath +
@@ -31,7 +57,7 @@ app.get("/api", (req, res) => {
           ".jpg"
       );
     });
-  }
+  } */
 });
 
 app.listen(port, () => {
